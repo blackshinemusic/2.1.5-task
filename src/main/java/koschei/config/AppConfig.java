80e25ddc429e04a78c19efad0f7b4c8c70c76deb
blackshinemusic <144614162+blackshinemusic@users.noreply.github.com>
2023-12-03 18:57:20 +0300

    .
    .

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,32 @@
+package koschei.config;
+
+import koschei.models.Deth8;
+import koschei.models.Island2;
+import koschei.models.Needle7;
+import koschei.models.Wood3;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public Needle7 needle7() {
+        Needle7 needle7 = new Needle7();
+        needle7.setDeth8(deth()); // Внедрение зависимости через сеттер
+        return needle7;
+    }
+
+    @Bean
+    public Deth8 deth() {
+        return new Deth8();
+    }
+}
---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+public class Needle7 {
+
+    private Deth8 deth;
+
+    public void setDeth8(Deth8 deth) {
+        this.deth = deth;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + deth.toString();
+    }
+}
